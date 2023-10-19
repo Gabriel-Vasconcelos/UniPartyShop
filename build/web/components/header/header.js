6d2a256a5a66a3d312bc39f58fa130260159b2b6
gabriel-vasconcelos <gabrieltotal6@gmail.com>
2023-10-19 19:09:56 -0300

    Fixing Header js

--- build/web/components/header/header.js
+++
@@ -1,9 +0,0 @@
-const openProfileOptions = () => {
-    const 
-}
-
-window.addEventListener('load', () => {
-    const profileButton = document.querySelector(".profile-icon")
-    
-    profileButton.addEventListener('click', openProfileOptions())
-})
